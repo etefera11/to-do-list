804d53a80584fe65a87edad667a91b388660c9ec
Ezana Tefera <etefera11@gmail.com>
2023-04-08 19:10:35 -0400

    removed unused images and added to do icon

--- to-do-list/src/components/TodoList.tsx
+++ to-do-list/src/components/TodoList.tsx
@@ -1,5 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './TodoList.css';
 
 type Item = {
     text: string;
@@ -32,23 +33,24 @@ function TodoList() {
 
     return (
         <div>
-          <h1>To-Do List</h1>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              placeholder="Add to-do item"
-              value={text}
-              onChange={handleChange}
-            />
-            <button className="btn btn-primary">Add Item</button>
-          </form>
-          <ul>
-            {items.map((item: Item) => (
-              <li key={item.id}>{item.text}</li>
-            ))}
-          </ul>
+            <img className="to-do-img" src="/to-do-list.png"></img>
+            <h1>To-Do List</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Add to-do item"
+                    value={text}
+                    onChange={handleChange}
+                />
+                <button className="btn btn-primary">Add Item</button>
+            </form>
+            <ul>
+                {items.map((item: Item) => (
+                    <li key={item.id}>{item.text}</li>
+                ))}
+            </ul>
         </div>
-      );
+    );
 }
 
 export default TodoList;
